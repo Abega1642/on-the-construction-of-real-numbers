9c3632501085f2d18027019b12999519448ee89b
Abega1642 <hei.abega.2@gmail.com>
2025-02-04 13:47:02 +0300

    feat: implement Fraction addition method

--- src/main/java/dev/razafindratelo/tools/Fraction.java
+++ src/main/java/dev/razafindratelo/tools/Fraction.java
@@ -15,7 +15,7 @@ public class Fraction implements Tool {
 
     public Fraction(int numerator, int denominator) {
         if (denominator == 0) {
-            throw new IllegalArgumentException("denominator must be greater than zero");
+            throw new IllegalArgumentException("denominator must be greater non-zero");
         }
         this.numerator = numerator;
         this.denominator = denominator;
@@ -29,6 +29,25 @@ public class Fraction implements Tool {
     }
 
     public Fraction add(Fraction frac) {
-        return null;
+        int denGCM = NaturalNumber.gcm(denominator, frac.getDenominator());
+
+        int leftSideMult = denGCM / this.denominator;
+        int rightSideMult = denGCM /frac.denominator;
+
+
+        this.denominator *= leftSideMult;
+        this.numerator *= leftSideMult;
+
+        frac.setDenominator(frac.getDenominator() * rightSideMult);
+        frac.setNumerator(frac.getNumerator() * rightSideMult);
+
+
+        var added = new Fraction(
+                this.numerator + frac.getNumerator(),
+                denGCM
+        );
+        added.simplify();
+
+        return added;
     }
 }
