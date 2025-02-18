c539fd0e75c2df2b946439cf17a342538023922b
Abega1642 <hei.abega.2@gmail.com>
2025-02-18 13:11:02 +0300

    feat: add SquareRoot sequence

---
+++ src/main/java/dev/razafindratelo/sequences/SquareRoot.java
@@ -0,0 +1,43 @@
+package dev.razafindratelo.sequences;
+
+import dev.razafindratelo.tools.Fraction;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This SquareRoot sequence is the sequence (x_n)
+ */
+@Getter
+@AllArgsConstructor
+public class SquareRoot {
+    private long n;
+    private List<Fraction> values;
+
+    public SquareRoot(long n) {
+        this.n = n;
+        this.values = new ArrayList<>();
+    }
+
+    public Fraction sq_(long k) {
+        SquareRootSub sqSub = new SquareRootSub(n);
+        if (k == 0) {
+            return new Fraction(sqSub.getRootValue());
+        } else {
+            Fraction val = Fraction.ZERO;
+            List<Fraction> values = new ArrayList<>();
+
+            for (int i = 1; i <= k; i++) {
+                Fraction sqSub_i = sqSub.sqSub_(i);
+                values.add(sqSub_i);
+            }
+            for (Fraction value : values) {
+                val = val.add(value.inverse().opposite());
+            }
+
+            return val.add(sqSub.getRootValue());
+        }
+    }
+}
