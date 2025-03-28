d0f44be16a344429e6081be655e1eb32b53c3601
Abega1642 <hei.abega.2@gmail.com>
2025-03-28 16:30:52 +0300

    feat(tool): add GMP and GMPUtils for GCD

---
+++ src/main/java/dev/razafindratelo/tools/gmp/GMP.java
@@ -0,0 +1,18 @@
+package dev.razafindratelo.tools.gmp;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import java.util.Collections;
+
+public interface GMP extends Library {
+
+    GMP INSTANCE = Native.load("gmp", GMP.class, Collections.emptyMap());
+
+    void __gmpz_init(Pointer x);
+    int __gmpz_set_str(Pointer x, String str, int base);
+    void __gmpz_gcd(Pointer result, Pointer a, Pointer b);
+    Pointer __gmpz_get_str(Pointer buffer, int base, Pointer x);
+    void __gmpz_clear(Pointer x);
+    void free(Pointer ptr);
+}
