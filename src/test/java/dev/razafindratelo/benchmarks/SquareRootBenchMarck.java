476e82b2f56e1438c1ab49b45780976bd7277a57
Abega1642 <hei.abega.2@gmail.com>
2025-07-11 11:45:16 +0300

    fixup

--- src/test/java/dev/razafindratelo/benchmarks/SquareRootBenchMarck.java
+++ src/test/java/dev/razafindratelo/benchmarks/SquareRootBenchMarck.java
@@ -10,22 +10,16 @@ public class SquareRootBenchMarck {
 
     public static String showResult(String title, long nat, long range, long precision, double time) {
 
-        return "[Benchmark of SquareRoot.kThValue] - " + title
-                        + "\n\t - nat\t\t= " + nat
-                        + "\n\t - range\t\t= " + range
-                        + "\n\t - precision\t= " + precision
-                        + "\n\t - duration\t= " + time + " seconds\n";
+        return "[Benchmark of SquareRoot.kThValue] - " + title + "\n\t - nat\t\t= " + nat + "\n\t - range\t\t= " + range
+                + "\n\t - precision\t= " + precision + "\n\t - duration\t= " + time + " seconds\n";
     }
 
     /**
-     * [Benchmark of SquareRoot.kThValue] - Fraction Form
-     *      - nat       = 2
-     *      - range     = 23
-     *      - precision = 6_000_002
-     *      - duration  = 226.88022827 second
+     * [Benchmark of SquareRoot.kThValue] - Fraction Form - nat = 2 - range = 23 - precision = 6_000_002 - duration =
+     * 226.88022827 second
      */
 
-    public static void sqrtOfTwoFractionForm(){
+    public static void sqrtOfTwoFractionForm() {
         var subject = SquareRoot.of(2);
         final long lengthPrecision = 6_000_002;
 
@@ -35,19 +29,14 @@ public class SquareRootBenchMarck {
 
         double durationInSeconds = (end - start) / 1_000_000_000.0;
 
-        System.out.println(
-                showResult("Fraction Form", 2, 23, lengthPrecision, durationInSeconds)
-        );
+        System.out.println(showResult("Fraction Form",2,23,lengthPrecision,durationInSeconds));
     }
 
     /**
-     *[Benchmark of SquareRoot.kThValue] - Decimal Form
-        - nat		= 2
-        - range		= 23
-        - precision	= 6000002
-        - duration	= 309.804193456 seconds
+     * [Benchmark of SquareRoot.kThValue] - Decimal Form - nat = 2 - range = 23 - precision = 6000002 - duration =
+     * 309.804193456 seconds
      */
-    public static void sqrtOfTwoDecimalForm(){
+    public static void sqrtOfTwoDecimalForm() {
         var subject = SquareRoot.of(2);
         final int lengthPrecision = 6_000_002;
         MathContext precision = new MathContext(lengthPrecision + 4);
@@ -58,9 +47,7 @@ public class SquareRootBenchMarck {
 
         double durationInSeconds = (end - start) / 1_000_000_000.0;
 
-        System.out.println(
-                showResult("Decimal Form", 2, 23, lengthPrecision, durationInSeconds)
-        );
+        System.out.println(showResult("Decimal Form",2,23,lengthPrecision,durationInSeconds));
     }
 
     public static void main(String[] args) throws IOException {
